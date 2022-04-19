7feefe0f8a780a5568a5c9b091c00e96a5339d57
ralph orteza <ralphlorteza@gmail.com>
2022-04-19 13:36:47 -0700

    Complete repeatStrings.js exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+  let out = "";
 
+  if (num < 0) { return 'ERROR';}
+  for (let i = 0; i < num; i++) {
+    out += str;
+  }
+  return out;
 };
 
 // Do not edit below this line
