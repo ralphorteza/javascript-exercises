0a5124a691012ce6fc1da7c44fae7b0b6abbad89
ralph orteza <ralphlorteza@gmail.com>
2022-04-19 15:01:08 -0700

    Complete sumAll.js exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) { return 'ERROR'; }
+  if (num1 < 0  || num2 < 0) { return 'ERROR'; }
+  
+  if (num1 > num2) {
+    let temp = num1;
+    num1 = num2;
+    num2 = temp;
+  }
+
+  let sum = 0;
+
+  while (num1 <= num2) {
+    sum += num1;
+    num1++;
+  }
+
+  return sum;
 
 };
 
