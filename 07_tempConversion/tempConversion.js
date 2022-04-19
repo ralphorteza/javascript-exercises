9907bbccb619a6aebd18a8200d48b0ef4f8ac875
ralph orteza <ralphlorteza@gmail.com>
2022-04-19 15:34:37 -0700

    Complete tempConversion.js exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(fehrenheitNum) {
+  let celsiusConversion =  5/9 * (fehrenheitNum - 32);
+  if (Number.isInteger(celsiusConversion)) { 
+    return parseInt(celsiusConversion.toFixed(0));
+  }
+  return parseFloat(celsiusConversion.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(celsiusNum) {
+  let fehrenheitConversion = 9 / 5 * celsiusNum + 32;
+  if (Number.isInteger(fehrenheitConversion)) { 
+    return parseInt(fehrenheitConversion.toFixed(0));
+  }
+  return parseFloat(fehrenheitConversion.toFixed(1));
 };
 
 // Do not edit below this line
