742833e5fceaf452f9b4a71604b741b2f7d34494
ralph orteza <ralphlorteza@gmail.com>
2022-06-28 19:03:11 -0700

    Complete 09 exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let processedStr = str.toLowerCase()
+                        .replace(/[^a-z]/g, "");
+  let test = '';
+  let size = processedStr.length;
+  for (let i = size-1; i >= 0; i--) {
+    test += processedStr.charAt(i);
+  }
 
+  return test === processedStr;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+/* function myFunction() {
+  let text;
+  let person = prompt("enter string");
+  if (person == null || person == "") {
+    return true;
+  } else {
+    return palindromes(person);
+  }
+}
+
+console.log(myFunction);
+ */
