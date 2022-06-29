f088a625a8d711d73f834153f7cf7169d9da056e
ralph orteza <ralphlorteza@gmail.com>
2022-06-28 17:52:05 -0700

    Complete 08 exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = (a,b) => a + b;
+
+const subtract = (a,b) => a - b;
+
+const sum = function(obj) {
+  let sum = 0;
+	for (key in obj) {
+    sum += obj[key]
+  }
+  return sum;
 };
 
-const subtract = function() {
-	
+const multiply = function(obj) {
+  if (obj === null) { return null; }
+  product = 1;
+  for (key in obj) {
+    product *= obj[key];
+  }
+  return product;
 };
 
-const sum = function() {
-	
-};
+const power = (a,b) => Math.pow(a,b);
 
-const multiply = function() {
 
-};
+const factorial = function(num) {
+	let out = num;
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+  if (num === 0 || num === 1) { return 1; }
+  while (num > 1) {
+    num--;
+    out *= num;
+  }
+  return out;
 };
 
 // Do not edit below this line
