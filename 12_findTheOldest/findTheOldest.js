fe1652d8127294cfa3284ac3fd49a7d3a7451b4b
ralph orteza <ralphlorteza@gmail.com>
2022-06-29 12:54:45 -0700

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,43 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((oldest, currentPerson) =>{
+    const oldestAge = yearsLived(oldest.yearOfDeath, oldest.yearOfBirth);
+    const currentAge = yearsLived(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
+    return (oldestAge > currentAge) ? oldest: currentPerson;
+  });
 };
 
+
+const yearsLived = ((deathYear, birthYear) => {
+  if (!deathYear) { deathYear = new Date().getFullYear(); }  
+  return deathYear - birthYear;
+});
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
+/* const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
+
+const getAges = people.sort(function(a,b) {
+  const lastGuy = a.yearOfDeath - a.yearOfBirth;
+  const nextGuy = b.yearOfDeath - b.yearOfBirth;
+  return (lastGuy > nextGuy) ? -1 : 1;
+})
+
+const getOldest = getAges.at(0);
+const getOldestName = getOldest.name; */
